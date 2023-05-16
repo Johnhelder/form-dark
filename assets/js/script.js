3d58eb8765553f304ca0f92649ea23c53f8e5834
John Helder <johnhelderjp@gmail.com>
2023-05-15 21:00:00 -0300

    Criando melhorias no script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
-const chk = document.getElementById('chk');
+// const chk = document.getElementById('chk');
 
-chk.addEventListener('change', () =>{
-  document.body.classList.toggle('dark');
+// chk.addEventListener('change', () =>{
+//   document.body.classList.toggle('dark');
+// });
+
+const form = document.getElementById('form');
+const tag = [...document.getElementsByTagName(form)];
+
+form.addEventListener('change', () =>{
+  document.tag.classList.toggle('dark');
 });
+
+// console.log(tag);
